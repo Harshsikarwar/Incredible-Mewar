30ed5288210befa93ce7788d6f2f2c90d8ad9a08
Harshsikarwar <harshsikarwar200@gmail.com>
2024-12-18 12:20:00 +0530

    Setting router

---
+++ Incredible-Mewar/src/components/Footer/footer.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import React from "react";
+
+export default function Header(){
+    return(
+        <h1>Footer</h1>
+    )
+
+}
--- Incredible-Mewar/src/main.jsx
+++ Incredible-Mewar/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Layout from './layout.jsx'
+import Home from './components/Home/Home.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter([{
+  path: '/',
+  element: <Layout/>,
+  children: [
+    {path: "",
+     element: <Home/>,
+    }
+  ]
+}])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
